7ad89f1d646fa4ee15cfcfa75f0debcce2a40bb2
Isika Powers <isika.powers@gmail.com>
2021-07-14 15:53:33 -0600

    Add tests for classes

---
+++ spec/joke_spec.rb
@@ -0,0 +1,14 @@
+require "./lib/joke"
+
+RSpec.describe Joke do
+
+  it "exists" do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke).to be_an_instance_of(Joke)
+    expect(joke.id).to eq(22)
+    expect(joke.setup).to eq("Why did the strawberry cross the road?")
+    expect(joke.punchline).to eq("Because his mother was in a jam.")
+  end
+
+end
