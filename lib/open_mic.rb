f95103e5d49e48769072b8db08d155a7fd1e4cb1
Isika Powers <isika.powers@gmail.com>
2021-07-14 15:55:46 -0600

    Update openmic class

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,8 +1,6 @@
 class OpenMic
 
-  attr_reader :location,
-              :date,
-              :performers
+  attr_reader :location, :date, :performers
 
   def initialize(attributes)
     @location = attributes[:location]
